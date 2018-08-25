4634fe4c8c6652cdef3fc7d2b56d715adfca6708
Mary Bork <maryebork@outlook.com>
2018-08-25 14:46:36 -0600

    Adds test_insert, test_count, test_suggest

--- test/complete_me_test.rb
+++ test/complete_me_test.rb
@@ -8,10 +8,32 @@ class CompleteMeTest < Minitest::Test
   def test_it_exists
     completion = CompleteMe.new
 
-    assert_instance_of CompleteMe, completion 
+    assert_instance_of CompleteMe, completion
   end
 
+  def test_insert
+    completion = CompleteMe.new
 
+    assert_equal "pizza", completion.insert("pizza")
+  end
+
+  def test_insert_count
+    completion = CompleteMe.new
+    completion.insert("pizza")
+    assert_equal 1, completion.count
+  end
+
+  def test_suggest
+    completion = CompleteMe.new
+    completion.insert("pizza")
+    assert_equal (["pizza"]), completion.suggest("piz")
+  end
+
+  # def test_dictionary
+  #   completion = CompleteMe.new
+  #   completion.populate(dictionary)
+  #   assert_equal 235886, completion.count
+  # end
 
 
 end
