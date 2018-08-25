33dc6978dc4c8261390d8dfc6cc9c8abe85a2895
Mary Bork <maryebork@outlook.com>
2018-08-25 13:46:07 -0600

    Adds CompleteMe Class Test

--- test/complete_me_test.rb
+++ test/complete_me_test.rb
@@ -1,2 +1,17 @@
-require
-class CompleteMe
+require 'minitest/autorun'
+require 'minitest/pride'
+require 'pry'
+require './lib/complete_me.rb'
+
+class CompleteMeTest < Minitest::Test
+
+  def test_it_exists
+    completion = CompleteMe.new
+
+    assert_instance_of CompleteMe, completion 
+  end
+
+
+
+
+end
