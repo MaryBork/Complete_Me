f220b12c15819a776ec2d9b8d138e679ab0fefb9
Mary Bork <maryebork@outlook.com>
2018-08-28 18:02:27 -0600

    WIP Suggest Method

--- lib/complete_me.rb
+++ lib/complete_me.rb
@@ -17,30 +17,25 @@ class CompleteMe
 #to a second hash pair(w/ defaulted value at 0)
 #the first key is a word prefix, the second key is
 #a chosen complete word and the value is a weight integer
+def insert(complete_word, current = @root)
+   first_letter = complete_word[0]
 
-<<<<<<< HEAD
-  def insert(complete_word, current=@root)
-    @whole_words = []
-    @whole_words << complete_word
-=======
-  def insert(complete_word, current = @root)
->>>>>>> 8b673b1aa1c7997e7d1da5131874b79846d670d9
-    first_letter = complete_word[0]
-    complete_word[0] = ("")
-    next_node = current.child(first_letter)
-      if
-        next_node == nil
-        next_node = Node.new(first_letter)
-        current.set_children(first_letter, next_node)
-      end
-      if
-        complete_word == ""
-        next_node.complete_word = true
-        return
-      end
-    insert(complete_word, next_node)
-  end
-#insert takes in a complete word and indicates
+   complete_word[0] = ("")
+   next_node = current.child(first_letter)
+     if
+       next_node == nil
+       next_node = Node.new(first_letter)
+       current.set_children(first_letter, next_node)
+     end
+     if
+       complete_word == ""
+       next_node.complete_word = true
+       return
+     end
+   insert(complete_word, next_node)
+ end
+
+# #insert takes in a complete word and indicates
 #by default its placement entry at root or current.
 #the complete word is entered into an empty array,
 #first letter is assigned to an empty string,
@@ -52,28 +47,6 @@ class CompleteMe
 #end method
 
 def count(current_node = @root)
- number_of_valid_words = current_node.children.inject(0) do |total, (char, child_node)|
-   total + count(child_node)
-   end
- return number_of_valid_words + 1 if current_node.complete_word?
- return number_of_valid_words
-end
-
-#goes to current_node and starting at the first(0) index of the array
-#counts each consecutive node and returns the total plus 1 to number_of_valid_words
-#Returns the number attached to number_of_valid_words
-
-
-  def populate(dictionary)
-     @words = dictionary.split("\n")
-      @words.map do |word|
-      insert(word.chomp)
-    end
-<<<<<<< HEAD
-=======
-  end 
-
-  def count(current_node = @root)
     number_of_valid_words = current_node.children.inject(0) do |total, (char, child_node)|
       total + count(child_node)
       end
@@ -81,69 +54,47 @@ end
     return number_of_valid_words
   end
 
-  def traverse
-    #   word = prefix.to_s.join 
-    #   current_node = @root
-    # until @child.complete_word == true 
-    #   word += current_node.value  
-    #   current_node = current_node.next_node 
-    #   return word 
-    #   traverse 
-    
->>>>>>> 8b673b1aa1c7997e7d1da5131874b79846d670d9
-  end
-#Takes a file in as an argument then splits the file up at the newline characters
-#creating an array of individual strings, then inserts(using #insert) each character of each string into
-#our retrieval tree as instances of Node
-
-
-  def suggest(prefix)
-<<<<<<< HEAD
-    node_path = [] << current_node
-    return nil if current_node.nil?
-    letters = prefix.chars
-    letters.each do |letter|
-      if current_node.value == letter
-        node_path << current_node
-      end
-
-# #   def suggest(prefix)
-# #   stash = []
-# #   words      = []
-# #   current_string = []
-# #   unvisited_nodes << current_string.include?(prefix)
-# #   current_string << prefix.chars.take(prefix.size-1)
-# #   unless prefix_paths(0)
-# #     return []
-# #   end
-# #   until prefix_paths.empty?
-# #     node = prefix_paths.pop
-# #     current_string.pop and next if current_node == :guard_node
-# #     current_string << current_node.value
-# #     prefix_paths      << :guard_node
-# #   end
-# #   if node.complete_word
-# #     words << current_string.join
-# #     current_node.child.each { |n| unvisited_nodes << n }
-# #   end
-# #   complete_words
-# # end
-=======
-    # array_of_prefix = prefix.chars
-    # bingo = array_of_prefix.map do |element|
-    #   element.to_sym 
-    # end 
-      word = prefix
-      current_node = @root
-      bank = []
-    until current_node.complete_word == true 
-      binding.pry
-      current_node = current_node.next_node 
-      word += current_node.value  
-    #suggest
+# # #goes to current_node and starting at the first(0) index of the array
+# # #counts each consecutive node and returns the total plus 1 to number_of_valid_words
+# # #Returns the number attached to number_of_valid_words
+# #
+# #
+  def populate(dictionary)
+     @words = dictionary.split("\n")
+      @words.map do |word|
+      insert(word.chomp)
     end
-    bank << word
-    #bingo
-    
->>>>>>> 8b673b1aa1c7997e7d1da5131874b79846d670d9
   end
+# #
+# #   def traverse
+# #     #   word = prefix.to_s.join
+# #     #   current_node = @root
+# #     # until @child.complete_word == true
+# #     #   word += current_node.value
+# #     #   current_node = current_node.next_node
+# #     #   return word
+# #     #   traverse
+# #
+# #   end
+# # # #Takes a file in as an argument then splits the file up at the newline characters
+# # # #creating an array of individual strings, then inserts(using #insert) each character of each string into
+# # # #our retrieval tree as instances of Node
+# #
+# #
+  def suggest(prefix, nodes = @root, array = [])
+
+    nodes.each do |element|
+
+      suggest(prefix += element.value)
+
+      if node.complete_word == true
+        array << prefix
+      end
+    end
+    array
+  end
+
+end
+#suggest takes in a prefix,
+#assigns prefix to 'word' variable,
+#assign 'current_node' variable
